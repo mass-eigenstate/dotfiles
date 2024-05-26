926b70fdccc5a0733dc4f154ab82d3318acb00fe
Mike Procario <procario@gmail.com>
2024-05-26 16:07:22 -0400

    Small fiuxes to my nvim config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,8 +1,8 @@
 return { 
     "nvim-lualine/lualine.nvim",
-    dependencies = "kyazdani42/nvim-web-devicons",
+    dependencies = "nvim-tree/nvim-web-devicons",
     config = true,
-    options = { theme = 'nightfly' }
+    opts = { theme = 'nightfly' }
 }
 
 
--- .config/nvim/lua/plugins/simple.lua
+++ .config/nvim/lua/plugins/simple.lua
@@ -2,5 +2,5 @@ return {
         "folke/neodev.nvim",
         {"folke/which-key.nvim", opts = {} },
         {"folke/neoconf.nvim", cmd = "Neoconf" },
-        {"numToStr/Comment.nvim", opt = {} },
+        {"numToStr/Comment.nvim", opts = {} },
     }
