4af02176d0c5015dd1e6e4cf102b0a344ebe41d1
Mike Procario <procario@gmail.com>
2024-05-26 14:13:44 -0400

    Forgot tthe keymaps.lus file. I have now added it

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = "Move to the bottom pane" })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = "Move to the top pane" })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = "Move to the left pane" })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = "Move to the right pane" })
