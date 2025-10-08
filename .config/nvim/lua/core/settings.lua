45254195a1a42c436ab240e1f472952ff92860d1
Michael Procario <procario@gmail.com>
2025-10-07 20:42:56 -0400

    Fixed merge conflict

--- .config/nvim/lua/core/settings.lua
+++ .config/nvim/lua/core/settings.lua
@@ -1,13 +1,13 @@
-vim.o.number = true 
+-- vim.o.have_nerd_font = true
+vim.o.number = true
 vim.o.relativenumber = true
 vim.o.autoindent = true
-vim.o.syntax = "on"
+vim.o.syntax = 'on'
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.expandtab = true
 vim.o.termguicolors = true -- True color support
-vim.o.wrap = false 
+vim.o.wrap = false
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
