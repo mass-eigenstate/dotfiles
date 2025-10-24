560f5ecb4c0d69a17779c08aa7fea7c86055ab93
Michael Procario <procario@gmail.com>
2025-10-23 20:52:33 -0400

    Change ghostty font size

--- .config/nvim/lua/plugins/simple.lua
+++
@@ -1,6 +0,0 @@
-return {
-        "folke/neodev.nvim",
-        {"folke/which-key.nvim", opts = {} },
-        {"folke/neoconf.nvim", cmd = "Neoconf" },
-        {"numToStr/Comment.nvim", opts = {} },
-    }
